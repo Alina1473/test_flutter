35f3454e52098a77fc479bdeb0bac63e3c626cd5
Alina1473 <alina1473@gmail.com>
2021-02-27 14:50:07 +0200

    fix homescreen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,90 +13,94 @@ class HomeScreen extends StatelessWidget {
     EventData eventData = EventData();
     return Scaffold(
       appBar: CustomAppBar(),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 100,
-                  child: Center(
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        IconSection(
-                          pathToIcon: "assets/img/Iconly-Bulk-Document.png",
-                          label: "Contract",
-                          notification: 2,
-                        ),
-                        IconSection(
-                          pathToIcon: "assets/img/Iconly-Bulk-Image.png",
-                          label: "Photos",
-                          notification: 0,
-                        ),
-                        IconSection(
-                          pathToIcon: "assets/img/Iconly-Bulk-Video.png",
-                          label: "Movies",
-                          notification: 12,
-                        ),
-                        IconSection(
-                          pathToIcon: "assets/img/Iconly-Bulk-Location.png",
-                          label: "Location",
-                          notification: 0,
-                        ),
-                        IconSection(
-                          pathToIcon: "assets/img/Iconly-Bulk-Participant.png",
-                          label: "Participant",
-                          notification: 0,
-                        ),
-                      ],
+      body: Column(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 100,
+                child: Center(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      IconSection(
+                        pathToIcon: "assets/img/Iconly-Bulk-Document.png",
+                        label: "Contract",
+                        notification: 2,
+                      ),
+                      IconSection(
+                        pathToIcon: "assets/img/Iconly-Bulk-Image.png",
+                        label: "Photos",
+                        notification: 0,
+                      ),
+                      IconSection(
+                        pathToIcon: "assets/img/Iconly-Bulk-Video.png",
+                        label: "Movies",
+                        notification: 12,
+                      ),
+                      IconSection(
+                        pathToIcon: "assets/img/Iconly-Bulk-Location.png",
+                        label: "Location",
+                        notification: 0,
+                      ),
+                      IconSection(
+                        pathToIcon: "assets/img/Iconly-Bulk-Participant.png",
+                        label: "Participant",
+                        notification: 0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverToBoxAdapter(
+                  child: LabelSchedules(label: 'Optional Schedules'),
+                ),
+                SliverToBoxAdapter(
+                  child: OptionalSchedules(),
+                ),
+                SliverToBoxAdapter(
+                  child: LabelSchedules(label: 'Active Schedules'),
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return EventCard(
+                        event: eventData.activeEvents[index],
+                      );
+                    },
+                    childCount: eventData.activeEvents.length,
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 18),
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 19),
+                      height: 1,
+                      color: Colors.grey[300],
                     ),
                   ),
                 ),
+                SliverToBoxAdapter(
+                  child: LabelSchedules(label: 'Completed Schedules'),
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return EventCard(
+                        event: eventData.completedEvents[index],
+                      );
+                    },
+                    childCount: eventData.completedEvents.length,
+                  ),
+                ),
               ],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: LabelSchedules(label: 'Optional Schedules'),
-          ),
-          SliverToBoxAdapter(
-            child: OptionalSchedules(),
-          ),
-          SliverToBoxAdapter(
-            child: LabelSchedules(label: 'Active Schedules'),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return EventCard(
-                  event: eventData.activeEvents[index],
-                );
-              },
-              childCount: eventData.activeEvents.length,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 18),
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 19),
-                height: 1,
-                color: Colors.grey[300],
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: LabelSchedules(label: 'Completed Schedules'),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return EventCard(
-                  event: eventData.completedEvents[index],
-                );
-              },
-              childCount: eventData.completedEvents.length,
             ),
           ),
         ],
